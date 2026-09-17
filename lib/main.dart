69cdb1c46236ec2bf677459ada58993840131784
kibbha <dispigno.geneva@gmail.com>
2026-09-17 22:11:31 +0200

    UI: enable Android edge-to-edge fullscreen layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,18 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'app/app.dart';
 
-void main() => runApp(const ReadySafeApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      systemNavigationBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarIconBrightness: Brightness.dark,
+      systemNavigationBarContrastEnforced: false,
+    ),
+  );
+  runApp(const ReadySafeApp());
+}
